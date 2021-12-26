a083f873bcbe1e7339ac3c443fe2cc3ada65f8fc
Ghulam <inferno2960@gmail.com>
2021-12-26 17:56:32 -0500

    changed the message model

--- models/message.model.js
+++ models/message.model.js
@@ -1,17 +1,9 @@
 const messageFunction = (sequelize, Sequelize) => {
     const Message = sequelize.define("message", {
-        user: {
-            type: Sequelize.STRING, 
-            allowNull: false
-        }, 
         message: {
             type: Sequelize.STRING, 
             allowNull: false
         }, 
-        groupID: {
-            type: Sequelize.STRING, 
-            allowNull: false
-        }
     });
 
     return Message; 
