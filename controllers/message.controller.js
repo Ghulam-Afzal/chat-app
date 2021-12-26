d5e2eff69ff2b6c2c081bebdc07c97124fac7bbf
Ghulam <inferno2960@gmail.com>
2021-12-26 17:57:27 -0500

    the returned json now includes the user who made the message and the group the message was sent in

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -3,8 +3,22 @@ const db = require('../models')
 const Messages = db.message
 
 // returns all messages sent in a group
-messageRouter.get("/getMessages/:id", async (req, res) => {
-    const messages = await Messages.findAll({})
+messageRouter.get("/getMessages", async (req, res) => {
+    const groupId = req.body.groupId
+
+    const messages = await Messages.findAll({
+        where: {
+            groupId: groupId
+        }, 
+        include: [ { 
+            model: db.user, 
+            attributes: [ "id", "username" ] 
+        },
+        { 
+            model: db.group, 
+            attributes: [ "id", "name", "groupId"]
+        } ]
+    })
 
     res.json(messages)
 })
