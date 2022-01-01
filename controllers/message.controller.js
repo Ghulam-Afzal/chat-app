2ab5c2366f567dd3f7b8c13c5fd609f3960fbd3e
Ghulam <inferno2960@gmail.com>
2022-01-01 16:59:53 -0500

    added a filter to return messages only for a group

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -3,13 +3,10 @@ const db = require('../models')
 const Messages = db.message
 
 // returns all messages sent in a group
-messageRouter.get("/getMessages", async (req, res) => {
+messageRouter.post("/getMessages", async (req, res) => {
     const groupId = req.body.groupId
 
     const messages = await Messages.findAll({
-        where: {
-            groupId: groupId
-        }, 
         include: [ { 
             model: db.user, 
             attributes: [ "id", "username" ] 
@@ -17,10 +14,11 @@ messageRouter.get("/getMessages", async (req, res) => {
         { 
             model: db.group, 
             attributes: [ "id", "name", "groupId"]
-        } ]
+        }],
     })
 
-    res.json(messages)
+    const groupMessages = messages.filter(groupMessage => groupId === groupMessage.dataValues.groupId)
+    res.json(groupMessages)
 })
 
 
